78139abd1cc4cd95021ba68fe92069ff1f310954
pxn <pxn123>
2018-09-28 18:27:11 +0800

    init

---
+++ src/views/page/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app-container">
+    <el-main class="el-main">
+      <p> I love vue</p>
+      <li class="decrease"> id: {{ id }}</li>
+      <li class="decrease">name: {{ name }}</li>
+      <li class="decrease">description: {{ desc }}</li>
+    </el-main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: '',
+      name: '',
+      desc: ''
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.id = '1'
+      this.name = 'i have no name'
+      this.desc = 'description'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .line{
+    text-align: center;
+  }
+</style>
